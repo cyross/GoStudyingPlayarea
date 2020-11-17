777b6c4e77aad64cda5e0b05ccc6cb2a306cf5e4
Cyross Makoto <takuan45189@gmail.com>
2020-11-17 09:52:59 +0900

    added comment

--- go009/channel2.go
+++ go009/channel2.go
@@ -2,14 +2,16 @@ package main
 
 import "fmt"
 
-func sendValue(c chan<- int) {
+// 単なる写経になっているので、検証コードを入れる
+
+func send(c chan<- int) {
 	for i := 0; i < 5; i++ {
 		c <- i
 	}
 	close(c)
 }
 
-func receiveValue(c <-chan int) {
+func receive(c <-chan int) {
 	for v := range c {
 		fmt.Println("チャネルから受け取った数:", v)
 	}
@@ -17,8 +19,8 @@ func receiveValue(c <-chan int) {
 
 func main() {
 	c := make(chan int)
-	go sendValue(c)
-	receiveValue(c)
+	go send(c)
+	receive(c)
 
 	fmt.Println("処理を終了します。")
 }
